dddb8f0da4cc19798f8c6e940d32ab8034b3f2df
ShadowTinker <shadowtinkery@gmail.com>
2024-01-07 23:34:04 +0800

    [feat] add meta model

---
+++ model/metamodel.py
@@ -0,0 +1,180 @@
+import os
+import wandb
+import torch
+import logging
+import time
+import evaluation
+from data.dataset import BaseDataset
+from copy import deepcopy
+
+from tqdm import tqdm
+from torch import optim
+from utils import get_model_class, MetaOptimizer
+from collections import defaultdict
+from model.loss_func import *
+from data.dataset import *
+from typing import Dict, List, Optional, Tuple
+
+from utils.utils import xavier_normal_initialization, normal_initialization
+
+from model.basemodel import BaseModel
+
+class MetaModel(BaseModel):
+    def __init__(self, config: Dict, dataset_list: List[BaseDataset]) -> None:
+        super().__init__(config, dataset_list)
+        self.interval = config['train']['interval']
+        self.step_counter = 0
+        self.item_embedding = None # MetaModel is just a trainer without item embedding
+
+    def _init_model(self, train_data):
+        self.sub_model : BaseModel = self._register_sub_model()
+        self.sub_model._init_model(train_data)
+        self.item_embedding = self.sub_model.item_embedding
+
+        self.meta_module : nn.Module = self._register_meta_modules()
+        self.meta_module = self.meta_module.to(self.device)
+        self.meta_module.apply(normal_initialization)
+
+        self.meta_optimizer = self._get_meta_optimizers()
+        self.metaloader_iter = iter(self.current_epoch_metaloaders(nepoch=0))
+
+    def _register_sub_model(self) -> BaseModel:
+        sub_model_config = deepcopy(self.config)
+        sub_model_config['model']['model'] = self.config['model']['sub_model']
+        model_class = get_model_class(sub_model_config['model'])
+        return model_class(sub_model_config, self.dataset_list)
+
+    def _register_meta_modules(self) -> nn.Module:
+        return nn.Sequential(
+            nn.Linear(self.embed_dim, self.embed_dim),
+            nn.ReLU(),
+            nn.Linear(self.embed_dim, 1),
+            nn.Sigmoid()
+        )
+
+    def _get_meta_optimizers(self):
+        opt_name = self.config['train']['meta_optimizer']
+        lr = self.config['train']['meta_learning_rate']
+        params = self.meta_module.parameters()
+
+        if opt_name.lower() == 'adam':
+            optimizer = optim.Adam(params, lr=lr)
+        elif opt_name.lower() == 'sgd':
+            optimizer = optim.SGD(params, lr=lr)
+        elif opt_name.lower() == 'adagrad':
+            optimizer = optim.Adagrad(params, lr=lr)
+        elif opt_name.lower() == 'rmsprop':
+            optimizer = optim.RMSprop(params, lr=lr)
+        elif opt_name.lower() == 'sparse_adam':
+            optimizer = optim.SparseAdam(params, lr=lr)
+        else:
+            optimizer = optim.Adam(params, lr=lr, weight_decay=1e-3)
+
+        optimizer = MetaOptimizer(optimizer, hpo_lr=1)
+
+        return optimizer
+
+    def forward(self, batch):
+        return self.sub_model.forward(batch)
+
+    def current_epoch_metaloaders(self, nepoch):
+        return self.dataset_list[0].get_loader()
+
+    def training_epoch(self, nepoch):
+        output_list = []
+
+        trn_dataloaders = self.current_epoch_trainloaders(nepoch)
+        trn_dataloaders = [trn_dataloaders]
+
+        for loader_idx, loader in enumerate(trn_dataloaders):
+            outputs = []
+            loader = tqdm(
+                loader,
+                total=len(loader),
+                ncols=75,
+                desc=f"Training {nepoch:>5}",
+                leave=False,
+            )
+            for batch_idx, batch in enumerate(loader):
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                batch['neg_item'] = self._neg_sampling(batch)
+                self.sub_model.optimizer.zero_grad()
+                training_step_args = {'batch': batch, 'sub_model': self.sub_model}
+                loss = self.training_step(**training_step_args)
+                loss.backward()
+                self.sub_model.optimizer.step()
+                outputs.append({f"loss_{loader_idx}": loss.detach()})
+                self.step_counter += 1
+                if self.step_counter % self.config['train']['interval'] == 0 and \
+                    nepoch > self.config['train']['warmup_epoch']:
+                    self._outter_loop(nepoch)
+            output_list.append(outputs)
+        return output_list
+
+    def _outter_loop(self, nepoch):
+        meta_dataloader = self.current_epoch_metaloaders(nepoch)
+
+        assert self.config['train']['descent_step'] < len(meta_dataloader)
+        loader = tqdm(
+            meta_dataloader,
+            total=self.config['train']['descent_step'],
+            ncols=75,
+            desc=f"Meta Training {nepoch:>5}",
+            leave=False,
+        )
+        proxy_model = self._register_sub_model().to(self.device)
+        proxy_model._init_model(self.dataset_list[0])
+        proxy_model.load_state_dict(self.sub_model.state_dict())
+        for batch_idx, batch in enumerate(loader):
+            if batch_idx >= self.config['train']['descent_step']:
+                break
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+            batch['neg_item'] = self._neg_sampling(batch)
+            training_step_args = {'batch': batch, 'sub_model': proxy_model}
+            loss = self.training_step(**training_step_args)
+            proxy_model.optimizer.zero_grad()
+            loss.backward()
+            proxy_model.optimizer.step()
+
+        try:
+            batch = next(self.metaloader_iter)
+        except StopIteration:
+            self.metaloader_iter = iter(self.current_epoch_metaloaders())
+            batch = next(self.metaloader_iter)
+        batch = {k: v.to(self.device) for k, v in batch.items()}
+        batch['neg_item'] = self._neg_sampling(batch)
+        training_step_args = {'batch': batch, 'sub_model': proxy_model}
+        meta_loss = self.training_step(**training_step_args)
+
+        trainloader = self.current_epoch_trainloaders(nepoch)
+        batch = next(iter(trainloader))
+        batch = {k: v.to(self.device) for k, v in batch.items()}
+        batch['neg_item'] = self._neg_sampling(batch)
+        training_step_args = {'batch': batch, 'sub_model': self.sub_model}
+        meta_train_loss = self.training_step(**training_step_args)
+
+        hyper_grads = self.meta_optimizer.step(
+            val_loss=meta_loss,
+            train_loss=meta_train_loss,
+            aux_params = list(self.meta_module.parameters()),
+            parameters = list(self.sub_model.parameters()),
+            return_grads = True,
+            entropy = None
+        )
+
+    def selection(self, query):
+        logits = self.meta_module(query)
+        return logits
+
+    def training_step(self, batch, sub_model):
+        query = self.forward(batch)
+        pos_score = (query * self.item_embedding.weight[batch[self.fiid]]).sum(-1)
+        neg_score = (query * self.item_embedding.weight[batch['neg_item']]).sum(-1)
+        pos_score[batch[self.fiid] == 0] = -torch.inf # padding
+
+        loss_value = sub_model.loss_fn(pos_score, neg_score, sum=False)
+        weight = self.selection(query).squeeze()
+        loss_value = loss_value.sum() * 0.5
+        # loss_value = (loss_value * weight).sum()
+
+        return loss_value
--- utils/utils.py
+++ utils/utils.py
@@ -6,6 +6,7 @@ import os
 import importlib
 import torch.nn as nn
 from copy import deepcopy
+from torch.nn.utils.clip_grad import clip_grad_norm_
 
 def seed_everything(seed=1111):
     random.seed(seed)
@@ -127,3 +128,140 @@ def transform_sweep_config_into_config(sweep_config):
         key = k.split('.')
         config[key[0]][key[1]] = v
     return config
+
+class Hypergrad:
+    """Implicit differentiation for auxiliary parameters.
+    This implementation follows the Algs. in "Optimizing Millions of Hyperparameters by Implicit Differentiation"
+    (https://arxiv.org/pdf/1911.02590.pdf), with small differences.
+
+    """
+
+    def __init__(self, learning_rate=.1, truncate_iter=3):
+        self.learning_rate = learning_rate
+        self.truncate_iter = truncate_iter
+
+    def grad(self, loss_val, loss_train, aux_params, params, entropy=None):
+        """Calculates the gradients w.r.t \phi dloss_aux/dphi, see paper for details
+
+        :param loss_val:
+        :param loss_train:
+        :param aux_params:
+        :param params:
+        :return:
+        """
+        dloss_val_dparams = torch.autograd.grad(
+            loss_val,
+            params,
+            retain_graph=True,
+            allow_unused=True
+        )
+
+        dloss_train_dparams = torch.autograd.grad(
+                loss_train,
+                params,
+                allow_unused=True,
+                create_graph=True,
+        )
+
+        v2 = self._approx_inverse_hvp(dloss_val_dparams, dloss_train_dparams, params)
+
+
+        if entropy is not None:
+            v_e = torch.autograd.grad(
+            entropy,
+            aux_params,
+            allow_unused=True,
+            retain_graph = True
+            )
+        v3 = torch.autograd.grad(
+            dloss_train_dparams,
+            aux_params,
+            grad_outputs=v2,
+            allow_unused=True
+        )
+
+        if entropy is None:
+        # note we omit dL_v/d_lambda since it is zero in our settings
+            return list(-g for g in v3)
+        else:
+            p1 = [curr_p - curr_v for (curr_p, curr_v) in zip(v_e, v3)]
+            return list(pp for pp in p1)
+
+
+    def _approx_inverse_hvp(self, dloss_val_dparams, dloss_train_dparams, params):
+        """
+
+        :param dloss_val_dparams: dL_val/dW
+        :param dloss_train_dparams: dL_train/dW
+        :param params: weights W
+        :return: dl_val/dW * dW/dphi
+        """
+        p = v = dloss_val_dparams
+
+        for _ in range(self.truncate_iter):
+            grad = torch.autograd.grad(
+                    dloss_train_dparams,
+                    params,
+                    grad_outputs=v,
+                    retain_graph=True,
+                    allow_unused=True
+                )
+
+            grad = [g * self.learning_rate for g in grad]  # scale: this a is key for convergence
+
+            v = [curr_v - curr_g for (curr_v, curr_g) in zip(v, grad)]
+            # note: different than the pseudo code in the paper
+            p = [curr_p + curr_v for (curr_p, curr_v) in zip(p, v)]
+
+        return list(pp for pp in p)
+
+class MetaOptimizer:
+
+    def __init__(self, meta_optimizer, hpo_lr, truncate_iter=3, max_grad_norm=10):
+        """Auxiliary parameters optimizer wrapper
+
+        :param meta_optimizer: optimizer for auxiliary parameters
+        :param hpo_lr: learning rate to scale the terms in the Neumann series
+        :param truncate_iter: number of terms in the Neumann series
+        :param max_grad_norm: max norm for grad clipping
+        """
+        self.meta_optimizer = meta_optimizer
+        self.hypergrad = Hypergrad(learning_rate=hpo_lr, truncate_iter=truncate_iter)
+        self.max_grad_norm = max_grad_norm
+
+    def step(self, train_loss, val_loss, parameters, aux_params, return_grads=False, entropy=None):
+        """
+
+        :param train_loss: train loader
+        :param val_loss:
+        :param parameters: parameters (main net)
+        :param aux_params: auxiliary parameters
+        :param return_grads: whether to return gradients
+        :return:
+        """
+        # zero grad
+        self.zero_grad()
+
+        # validation loss
+        hyper_gards = self.hypergrad.grad(
+            loss_val=val_loss,
+            loss_train=train_loss,
+            aux_params=aux_params,
+            params=parameters,
+            entropy=entropy
+        )
+
+        for p, g in zip(aux_params, hyper_gards):
+            p.grad = g
+
+        # grad clipping
+        if self.max_grad_norm is not None:
+            clip_grad_norm_(aux_params, max_norm=self.max_grad_norm)
+
+        # meta step
+        self.meta_optimizer.step()
+        if return_grads:
+            return hyper_gards
+
+    def zero_grad(self):
+        self.meta_optimizer.zero_grad()
