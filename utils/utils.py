924660f29ed2d7b6964f75a3453e4d8db8d14813
ShadowTinker <ymjandlyf@mail.ustc.edu.cn>
2024-01-14 09:08:24 +0800

    [feat] add gumbel topk selector

--- utils/utils.py
+++ utils/utils.py
@@ -251,3 +251,36 @@ class MetaOptimizer:
 
     def zero_grad(self):
         self.meta_optimizer.zero_grad()
+
+class SubsetOperator(torch.nn.Module):
+    def __init__(self, k, tau=1.0, hard=False, eps=1e-10):
+        super(SubsetOperator, self).__init__()
+        self.k = k
+        self.hard = hard
+        self.tau = tau
+        self.eps = eps
+
+    def forward(self, scores):
+        m = torch.distributions.gumbel.Gumbel(torch.zeros_like(scores), torch.ones_like(scores))
+        g = m.sample()
+        scores = scores + g
+
+        # continuous top k
+        khot = torch.zeros_like(scores)
+        onehot_approx = torch.zeros_like(scores)
+        for i in range(self.k):
+            khot_mask = torch.max(1.0 - onehot_approx, torch.tensor([self.eps]).cuda())
+            scores = scores + torch.log(khot_mask)
+            onehot_approx = torch.nn.functional.softmax(scores / self.tau, dim=1)
+            khot = khot + onehot_approx
+
+        if self.hard:
+            # straight through
+            khot_hard = torch.zeros_like(khot)
+            val, ind = torch.topk(khot, self.k, dim=1)
+            khot_hard = khot_hard.scatter_(1, ind, 1)
+            res = khot_hard - khot.detach() + khot
+        else:
+            res = khot
+
+        return res
