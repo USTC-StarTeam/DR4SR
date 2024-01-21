e3b80efec8d40f774c20317a1069d59612899870
ShadowTinker <shadowtinery@gmail.com>
2024-01-21 10:16:35 +0800

    [feat] add MetaModel5 and change SASRec5

--- model/sasrec5.py
+++ model/sasrec5.py
@@ -24,15 +24,28 @@ class SASRec5(BaseModel):
             config['model']['layer_num'],
             self.item_embedding,
         )
-        self.query_encoder_twin = deepcopy(self.query_encoder)
-        for param in self.query_encoder_twin.parameters():
-            param.requires_grad = False
+        self.query_encoder_twin = SASRecQueryEncoder(
+            self.fiid,
+            self.embed_dim,
+            self.max_seq_len,
+            config['model']['head_num'],
+            config['model']['hidden_size'],
+            config['model']['dropout_rate'],
+            config['model']['activation'],
+            config['model']['layer_norm_eps'],
+            config['model']['layer_num'],
+            self.item_embedding,
+        )
+        # self.query_encoder_twin = deepcopy(self.query_encoder)
+        # for param in self.query_encoder_twin.parameters():
+        #     param.requires_grad = False
+        self.L = 2
         self.condition_proj = nn.Sequential(
             nn.Linear(self.embed_dim, self.embed_dim),
             nn.ReLU(),
-            nn.Linear(self.embed_dim, 10 * self.embed_dim)
+            nn.Linear(self.embed_dim, self.L * self.embed_dim),
         )
-        # self.condition_proj = nn.Linear(self.embed_dim, 10 * self.embed_dim)
+        # self.condition_proj = nn.Linear(self.embed_dim, self.L * self.embed_dim)
 
     def current_epoch_trainloaders(self, nepoch):
         return super().current_epoch_trainloaders(nepoch)
@@ -40,11 +53,12 @@ class SASRec5(BaseModel):
     def forward(self, batch, need_pooling=True):
         query = self.query_encoder(batch, need_pooling=True)
         B, D = query.shape
-        query_c = self.condition_proj(query).reshape(B, 10, D)
-        self.query_encoder_twin.load_state_dict(self.query_encoder.state_dict())
+        query_c = self.condition_proj(query).reshape(B, self.L, D)
+        # query_c = query.reshape(B, 1, D)
+        # self.query_encoder_twin.load_state_dict(self.query_encoder.state_dict())
         batch = {
             'seq_emb': query_c,
-            'seqlen': 10 * torch.ones(B, dtype=torch.int64, device=self.device),
+            'seqlen': self.L * torch.ones(B, dtype=torch.int64, device=self.device),
         }
         query_q = self.query_encoder_twin(batch, need_pooling=True)
         self.query = query
@@ -65,8 +79,8 @@ class SASRec5(BaseModel):
         query_q = self.forward(batch)
         alignment = 0
         # alignment += self.alignment(self.query, query_q)
-        # alignment += 0.5 * self.alignment(query, self.item_embedding.weight[batch[self.fiid]])
-        alignment += self.alignment(query_q, self.item_embedding.weight[batch[self.fiid]])
+        alignment += 0.5 * self.alignment(self.query, self.item_embedding.weight[batch[self.fiid]])
+        alignment += 0.5 * self.alignment(query_q, self.item_embedding.weight[batch[self.fiid]])
         uniformity = self.config['model']['uniformity'] * (
             self.uniformity(self.query) +
             self.uniformity(query_q) +
@@ -74,11 +88,6 @@ class SASRec5(BaseModel):
         )
         loss_value = alignment + uniformity
         return loss_value
-
-    # def training_step(self, batch, reduce=True, return_query=False):
-    #     loss_value = super().training_step(batch, reduce=True, return_query=False)
-    #     loss_value = loss_value + self.embedding_loss
-    #     return loss_value
 
     def training_epoch(self, nepoch):
         output_list = []
@@ -106,40 +115,4 @@ class SASRec5(BaseModel):
                 self.optimizer.step()
                 outputs.append({f"loss_{loader_idx}": loss.detach()})
             output_list.append(outputs)
-        return output_list
-
-    @torch.no_grad()
-    def validation_epoch(self, nepoch, dataloader):
-        output_list = []
-        dataloader = tqdm(
-            dataloader,
-            total=len(dataloader),
-            ncols=75,
-            desc=f"Evaluating {nepoch:>5}",
-            leave=False,
-        )
-        for batch in dataloader:
-            batch = {k: v.to(self.device) for k, v in batch.items()}
-            batch['nepoch'] = nepoch
-            # model validation results
-            output = self.validation_step(batch)
-            output_list.append(output)
-        return output_list
-
-    @torch.no_grad()
-    def test_epoch(self, dataloader):
-        output_list = []
-        dataloader = tqdm(
-            dataloader,
-            total=len(dataloader),
-            ncols=75,
-            desc=f"Testing: ",
-            leave=False,
-        )
-        for batch in dataloader:
-            batch = {k: v.to(self.device) for k, v in batch.items()}
-            batch['nepoch'] = 1000
-            # model validation results
-            output = self.test_step(batch)
-            output_list.append(output)
         return output_list
