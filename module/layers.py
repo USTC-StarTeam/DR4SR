fe11324a08f07343fb02a62f546b14aa2a116903
ShadowTinker <shadowtinkery@gmail.com>
2024-01-23 23:09:20 +0800

    [feat] add SASRec7

--- module/layers.py
+++ module/layers.py
@@ -1,8 +1,10 @@
+import copy
+import math
 from typing import Tuple
 import torch
 import torch.nn as nn
-from torch.nn.parameter import Parameter
-from copy import deepcopy
+import torch.nn.functional as fn
+from torch.nn.init import normal_
 
 class SeqPoolingLayer(nn.Module):
     def __init__(self, pooling_type='mean', keepdim=False) -> None:
@@ -281,7 +283,7 @@ class VectorQuantizer(nn.Module):
         z_residual = z_flatten.reshape(-1, self.N, 1, self.e_dim)
         z_q = []
         for _ in range(self.depth):
-            d = torch.cdist(z_residual, embedding).squeeze()
+            d = torch.cdist(z_residual, embedding).reshape(B, self.N, self.K)
 
             # find closest encodings
             min_encoding_indices = torch.argmin(d, dim=-1).unsqueeze(-1)
@@ -326,7 +328,7 @@ class VectorQuantizer(nn.Module):
 
         return loss, z_q, perplexity, min_encodings, min_encoding_indices
 
-class SeqVectorQuantizer(VectorQuantizer):
+class VanillaVectorQuantizer(nn.Module):
     """
     Discretization bottleneck part of the VQ-VAE.
 
@@ -336,15 +338,17 @@ class SeqVectorQuantizer(VectorQuantizer):
     - beta : commitment cost used in loss term, beta * ||z_e(x)-sg[e]||^2
     """
 
-    # def __init__(self, dim, N, L, K, beta=0.25, depth=1, device='cuda'):
-    def __init__(self, dim, N, K, query_encoder, beta=0.25, depth=1,  device='cuda'):
-        super().__init__(dim, N, K, beta, depth, device)
-        self.query_encoder = query_encoder
-        self.query_encoder_twin = deepcopy(self.query_encoder)
-        for param in self.query_encoder_twin.parameters():
-            param.requires_grad = False
+    def __init__(self, n_e, e_dim, beta, depth=4):
+        super(VanillaVectorQuantizer, self).__init__()
+        self.n_e = n_e
+        self.e_dim = e_dim
+        self.beta = beta
+        self.depth = depth
 
-    def forward(self, z):
+        self.embedding = nn.Embedding(self.n_e, self.e_dim, padding_idx=0)
+        self.embedding.weight.data.uniform_(-1.0 / self.n_e, 1.0 / self.n_e)
+
+    def forward(self, z, device='cuda'):
         """
         Inputs the output of the encoder network z and maps it to a discrete 
         one-hot vector that is the index of the closest embedding vector e_j
@@ -359,51 +363,377 @@ class SeqVectorQuantizer(VectorQuantizer):
             2. flatten input to (B*H*W,C)
 
         """
-        B, D = z.shape
-
-        z_residual = z.reshape(B, self.N, 1, self.e_dim)
+        z_residual = z
         z_q = []
-        self.query_encoder_twin.load_state_dict(self.query_encoder.state_dict())
         for _ in range(self.depth):
-            d = torch.cdist(z_residual, self.embedding).squeeze()
+            d = torch.cdist(z_residual, self.embedding.weight)
+            min_encoding_indices = torch.argmin(d, dim=-1)
+            quantization_emb = self.embedding(min_encoding_indices)
 
-            # find closest encodings
-            min_encoding_indices = torch.argmin(d, dim=-1).unsqueeze(-1)
-            min_encodings = torch.zeros(
-                min_encoding_indices.shape[0],
-                self.N,
-                self.K,
-            ).to(z.device)
-            min_encodings = min_encodings.scatter(-1, min_encoding_indices, 1)
-
-            # get quantized latent vectors
-            quantization_emb = torch.einsum('BNK,NKD->BND', min_encodings, self.embedding)
-            z_residual = z_residual - quantization_emb.unsqueeze(-2)
-            quantization_emb = quantization_emb.flatten(-2, -1)
+            z_residual = z_residual - quantization_emb
             z_q.append(quantization_emb)
         z_q_residual = torch.stack(z_q, dim=1)
-        batch = {
-            'seq_emb': z_q_residual,
-            'seqlen': self.depth * torch.ones(B, dtype=torch.int64, device=z.device),
-        }
-        z_q = self.query_encoder_twin(batch, need_pooling=True)
-        # z_q = z_q_residual.sum(1)
+        z_q = z_q_residual.sum(1)
 
         # compute loss for embedding
         loss = torch.mean((z_q.detach()-z)**2) + self.beta * \
             torch.mean((z_q - z.detach()) ** 2)
-        
-        # loss = 0
-        # for d in range(self.depth):
-        #     loss += torch.cdist(z, z_q_residual[:, 0:d+1].sum(1).detach()).mean()
-        # loss = loss / self.depth
 
         # preserve gradients
         z_q = z + (z_q - z).detach()
 
-        # perplexity
-        # e_mean = torch.mean(min_encodings.flatten(0, 1), dim=0)
-        # perplexity = torch.exp(-torch.sum(e_mean * torch.log(e_mean + 1e-10)))
-        perplexity = 0
+        return loss, z_q
 
-        return loss, z_q, perplexity, min_encodings, min_encoding_indices
+
+
+
+class MLPLayers(nn.Module):
+    r"""MLPLayers
+
+    Args:
+        - layers(list): a list contains the size of each layer in mlp layers
+        - dropout(float): probability of an element to be zeroed. Default: 0
+        - activation(str): activation function after each layer in mlp layers. Default: 'relu'.
+                           candidates: 'sigmoid', 'tanh', 'relu', 'leekyrelu', 'none'
+
+    Shape:
+
+        - Input: (:math:`N`, \*, :math:`H_{in}`) where \* means any number of additional dimensions
+          :math:`H_{in}` must equal to the first value in `layers`
+        - Output: (:math:`N`, \*, :math:`H_{out}`) where :math:`H_{out}` equals to the last value in `layers`
+
+    Examples::
+
+        >>> m = MLPLayers([64, 32, 16], 0.2, 'relu')
+        >>> input = torch.randn(128, 64)
+        >>> output = m(input)
+        >>> print(output.size())
+        >>> torch.Size([128, 16])
+    """
+
+    def __init__(
+        self,
+        layers,
+        dropout=0.0,
+        activation="relu",
+        bn=False,
+        init_method=None,
+        last_activation=True,
+    ):
+        super(MLPLayers, self).__init__()
+        self.layers = layers
+        self.dropout = dropout
+        self.activation = activation
+        self.use_bn = bn
+        self.init_method = init_method
+
+        mlp_modules = []
+        for idx, (input_size, output_size) in enumerate(
+            zip(self.layers[:-1], self.layers[1:])
+        ):
+            mlp_modules.append(nn.Dropout(p=self.dropout))
+            mlp_modules.append(nn.Linear(input_size, output_size))
+            if self.use_bn:
+                mlp_modules.append(nn.BatchNorm1d(num_features=output_size))
+            activation_func = activation_layer(self.activation, output_size)
+            if activation_func is not None:
+                mlp_modules.append(activation_func)
+        if self.activation is not None and not last_activation:
+            mlp_modules.pop()
+        self.mlp_layers = nn.Sequential(*mlp_modules)
+        if self.init_method is not None:
+            self.apply(self.init_weights)
+
+    def init_weights(self, module):
+        # We just initialize the module with normal distribution as the paper said
+        if isinstance(module, nn.Linear):
+            if self.init_method == "norm":
+                normal_(module.weight.data, 0, 0.01)
+            if module.bias is not None:
+                module.bias.data.fill_(0.0)
+
+    def forward(self, input_feature):
+        return self.mlp_layers(input_feature)
+
+
+def activation_layer(activation_name="relu", emb_dim=None):
+    """Construct activation layers
+
+    Args:
+        activation_name: str, name of activation function
+        emb_dim: int, used for Dice activation
+
+    Return:
+        activation: activation layer
+    """
+    if activation_name is None:
+        activation = None
+    elif isinstance(activation_name, str):
+        if activation_name.lower() == "sigmoid":
+            activation = nn.Sigmoid()
+        elif activation_name.lower() == "tanh":
+            activation = nn.Tanh()
+        elif activation_name.lower() == "relu":
+            activation = nn.ReLU()
+        elif activation_name.lower() == "leakyrelu":
+            activation = nn.LeakyReLU()
+        elif activation_name.lower() == "dice":
+            activation = Dice(emb_dim)
+        elif activation_name.lower() == "none":
+            activation = None
+    elif issubclass(activation_name, nn.Module):
+        activation = activation_name()
+    else:
+        raise NotImplementedError(
+            "activation function {} is not implemented".format(activation_name)
+        )
+
+    return activation
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multi-head Self-attention layers, a attention score dropout layer is introduced.
+
+    Args:
+        input_tensor (torch.Tensor): the input of the multi-head self-attention layer
+        attention_mask (torch.Tensor): the attention mask for input tensor
+
+    Returns:
+        hidden_states (torch.Tensor): the output of the multi-head self-attention layer
+
+    """
+
+    def __init__(
+        self,
+        n_heads,
+        hidden_size,
+        hidden_dropout_prob,
+        attn_dropout_prob,
+        layer_norm_eps,
+    ):
+        super(MultiHeadAttention, self).__init__()
+        if hidden_size % n_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (hidden_size, n_heads)
+            )
+
+        self.num_attention_heads = n_heads
+        self.attention_head_size = int(hidden_size / n_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+        self.sqrt_attention_head_size = math.sqrt(self.attention_head_size)
+
+        self.query = nn.Linear(hidden_size, self.all_head_size)
+        self.key = nn.Linear(hidden_size, self.all_head_size)
+        self.value = nn.Linear(hidden_size, self.all_head_size)
+
+        self.softmax = nn.Softmax(dim=-1)
+        self.attn_dropout = nn.Dropout(attn_dropout_prob)
+
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.LayerNorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.out_dropout = nn.Dropout(hidden_dropout_prob)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (
+            self.num_attention_heads,
+            self.attention_head_size,
+        )
+        x = x.view(*new_x_shape)
+        return x
+
+    def forward(self, input_tensor, attention_mask):
+        mixed_query_layer = self.query(input_tensor)
+        mixed_key_layer = self.key(input_tensor)
+        mixed_value_layer = self.value(input_tensor)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer).permute(0, 2, 1, 3)
+        key_layer = self.transpose_for_scores(mixed_key_layer).permute(0, 2, 3, 1)
+        value_layer = self.transpose_for_scores(mixed_value_layer).permute(0, 2, 1, 3)
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer)
+
+        attention_scores = attention_scores / self.sqrt_attention_head_size
+        # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
+        # [batch_size heads seq_len seq_len] scores
+        # [batch_size 1 1 seq_len]
+        attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = self.softmax(attention_scores)
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+
+        attention_probs = self.attn_dropout(attention_probs)
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        hidden_states = self.dense(context_layer)
+        hidden_states = self.out_dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
+
+
+class FeedForward(nn.Module):
+    """
+    Point-wise feed-forward layer is implemented by two dense layers.
+
+    Args:
+        input_tensor (torch.Tensor): the input of the point-wise feed-forward layer
+
+    Returns:
+        hidden_states (torch.Tensor): the output of the point-wise feed-forward layer
+
+    """
+
+    def __init__(
+        self, hidden_size, inner_size, hidden_dropout_prob, hidden_act, layer_norm_eps
+    ):
+        super(FeedForward, self).__init__()
+        self.dense_1 = nn.Linear(hidden_size, inner_size)
+        self.intermediate_act_fn = self.get_hidden_act(hidden_act)
+
+        self.dense_2 = nn.Linear(inner_size, hidden_size)
+        self.LayerNorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+
+    def get_hidden_act(self, act):
+        ACT2FN = {
+            "gelu": self.gelu,
+            "relu": fn.relu,
+            "swish": self.swish,
+            "tanh": torch.tanh,
+            "sigmoid": torch.sigmoid,
+        }
+        return ACT2FN[act]
+
+    def gelu(self, x):
+        """Implementation of the gelu activation function.
+
+        For information: OpenAI GPT's gelu is slightly different (and gives slightly different results)::
+
+            0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+        Also see https://arxiv.org/abs/1606.08415
+        """
+        return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+
+    def swish(self, x):
+        return x * torch.sigmoid(x)
+
+    def forward(self, input_tensor):
+        hidden_states = self.dense_1(input_tensor)
+        hidden_states = self.intermediate_act_fn(hidden_states)
+
+        hidden_states = self.dense_2(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
+
+
+class TransformerLayer(nn.Module):
+    """
+    One transformer layer consists of a multi-head self-attention layer and a point-wise feed-forward layer.
+
+    Args:
+        hidden_states (torch.Tensor): the input of the multi-head self-attention sublayer
+        attention_mask (torch.Tensor): the attention mask for the multi-head self-attention sublayer
+
+    Returns:
+        feedforward_output (torch.Tensor): The output of the point-wise feed-forward sublayer,
+                                           is the output of the transformer layer.
+
+    """
+
+    def __init__(
+        self,
+        n_heads,
+        hidden_size,
+        intermediate_size,
+        hidden_dropout_prob,
+        attn_dropout_prob,
+        hidden_act,
+        layer_norm_eps,
+    ):
+        super(TransformerLayer, self).__init__()
+        self.multi_head_attention = MultiHeadAttention(
+            n_heads, hidden_size, hidden_dropout_prob, attn_dropout_prob, layer_norm_eps
+        )
+        self.feed_forward = FeedForward(
+            hidden_size,
+            intermediate_size,
+            hidden_dropout_prob,
+            hidden_act,
+            layer_norm_eps,
+        )
+
+    def forward(self, hidden_states, attention_mask):
+        attention_output = self.multi_head_attention(hidden_states, attention_mask)
+        feedforward_output = self.feed_forward(attention_output)
+        return feedforward_output
+
+
+class TransformerEncoder(nn.Module):
+    r"""One TransformerEncoder consists of several TransformerLayers.
+
+    Args:
+        n_layers(num): num of transformer layers in transformer encoder. Default: 2
+        n_heads(num): num of attention heads for multi-head attention layer. Default: 2
+        hidden_size(num): the input and output hidden size. Default: 64
+        inner_size(num): the dimensionality in feed-forward layer. Default: 256
+        hidden_dropout_prob(float): probability of an element to be zeroed. Default: 0.5
+        attn_dropout_prob(float): probability of an attention score to be zeroed. Default: 0.5
+        hidden_act(str): activation function in feed-forward layer. Default: 'gelu'
+                      candidates: 'gelu', 'relu', 'swish', 'tanh', 'sigmoid'
+        layer_norm_eps(float): a value added to the denominator for numerical stability. Default: 1e-12
+
+    """
+
+    def __init__(
+        self,
+        n_layers=2,
+        n_heads=2,
+        hidden_size=64,
+        inner_size=256,
+        hidden_dropout_prob=0.5,
+        attn_dropout_prob=0.5,
+        hidden_act="gelu",
+        layer_norm_eps=1e-12,
+    ):
+        super(TransformerEncoder, self).__init__()
+        layer = TransformerLayer(
+            n_heads,
+            hidden_size,
+            inner_size,
+            hidden_dropout_prob,
+            attn_dropout_prob,
+            hidden_act,
+            layer_norm_eps,
+        )
+        self.layer = nn.ModuleList([copy.deepcopy(layer) for _ in range(n_layers)])
+
+    def forward(self, hidden_states, attention_mask, output_all_encoded_layers=True):
+        """
+        Args:
+            hidden_states (torch.Tensor): the input of the TransformerEncoder
+            attention_mask (torch.Tensor): the attention mask for the input hidden_states
+            output_all_encoded_layers (Bool): whether output all transformer layers' output
+
+        Returns:
+            all_encoder_layers (list): if output_all_encoded_layers is True, return a list consists of all transformer
+            layers' output, otherwise return a list only consists of the output of last transformer layer.
+
+        """
+        all_encoder_layers = []
+        for layer_module in self.layer:
+            hidden_states = layer_module(hidden_states, attention_mask)
+            if output_all_encoded_layers:
+                all_encoder_layers.append(hidden_states)
+        if not output_all_encoded_layers:
+            all_encoder_layers.append(hidden_states)
+        return all_encoder_layers
+
